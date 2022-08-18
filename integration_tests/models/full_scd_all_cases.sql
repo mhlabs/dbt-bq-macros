7d7fe519104deb86e16a699dd807ccb0a6e3197a
Jesper Dejby <jesper.dejby@mathem.se>
2022-08-18 15:35:19 +0200

    old version of full scd

--- integration_tests/models/full_scd_all_cases.sql
+++ integration_tests/models/full_scd_all_cases.sql
@@ -1,26 +1,14 @@
 {{
     config(
-        materialized = 'incremental'
+        materialized = 'table'
     )
 }}
-
 WITH source AS (
     SELECT
         *
     FROM
         {{ ref('full_scd_all_cases_seed') }}
 ),
-
-
-temp_scd_source AS (
-    SELECT
-        *
-    FROM
-        {{ ref('temp_scd_all_cases_seed') }}
-),
-
-
-{% if not is_incremental() %}
 
 scd as (
     {{dbt_bq_macros.log_to_scd('source', 'product_id', 'event_id', 'updated_at', 'updated_at, event_id')}}
@@ -31,35 +19,3 @@ select
     {{ dbt_utils.surrogate_key(['product_id', 'valid_from']) }} AS scd_id
 from
 scd
-
-{% else %}
-
-full_scd AS (
-    select
-        *
-    from {{ this }}
-),
-
-temp_scd_ as (
-    {{dbt_bq_macros.log_to_scd('temp_scd_source', 'product_id', 'event_id', 'updated_at', 'updated_at, event_id')}}
-),
-
-temp_scd as (
-    select
-        *,
-        {{ dbt_utils.surrogate_key(['product_id', 'valid_from']) }} AS scd_id
-    from
-    temp_scd_
-),
-
-merge_scd AS (
-    {{dbt_bq_macros.log_to_scd_incremental('full_scd', 'temp_scd', 'product_id')}}
-)
-
-
-select
-    *
-from
-merge_scd
-
-{% endif %}
