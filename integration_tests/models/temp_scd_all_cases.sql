d3c40cb74a62bd321715778b2e00c45a01cec325
Jesper Dejby <jesper.dejby@mathem.se>
2022-08-18 15:33:55 +0200

    all use cases files and sql

---
+++ integration_tests/models/temp_scd_all_cases.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized = 'table'
+    )
+}}
+WITH source AS (
+    SELECT
+        *
+    FROM
+        {{ ref('temp_scd_all_cases_seed') }}
+),
+
+scd as (
+    {{dbt_bq_macros.log_to_scd('source', 'product_id', 'event_id', 'updated_at', 'updated_at, event_id')}}
+)
+
+select
+    *,
+    {{ dbt_utils.surrogate_key(['product_id', 'valid_from']) }} AS scd_id
+from
+scd
